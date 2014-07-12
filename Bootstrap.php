fe2797adf493f654f29848a72c74504be77858cc
Kaiser Franz Josef <wecodemore@gmail.com>
2014-07-12 17:30:43 +0200

    Initial commit to GitHub

---
+++ Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WCM;
+
+/**
+ * Plugin Name: (WCM) AstroFields Post Meta
+ * Description: Post Meta Data Extension for the AstroFields framework
+ */
+
+// Drop in Composer autoloader
+require_once plugin_dir_path( __FILE__ )."vendor/autoload.php";
